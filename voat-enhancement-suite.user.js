254b2ce2a88de27430d3f4e5d0df98b48dabda04
Travis Grammer <travisjgrammer@gmail.com>
2015-05-14 22:14:28 -0400

    many changes (see description)
    * moved basic utils outside of VESUtils
    
    + added comment page regex
    + substantiated isPageType(), isMatchURL() and matchesPageLocation()
    + skeleton for VoatingNeverEnds module

--- voat-enhancement-suite.user.js
+++ voat-enhancement-suite.user.js
@@ -17,8 +17,48 @@
 
 var VESversion = 0.01;
 
-// TODO - Move basic infos about pages into VESUtils
-// TODO - Move basic utils into VESUtils
+
+// some basic utils
+function hasClass(e,c) {
+    if ((typeof(e) == 'undefined') || (e == null)) {
+        console.log(arguments,callee,caller);
+        return false;
+    }
+    return ele.className.match(new RegExp('(\\s|^)'+c+'(\\s|$)'));
+};
+function addClass(e,c) {
+    if (!hasClass(e,c)) e.className += " "+c;
+};
+function removeClass(e,c) {
+    if (hasClass(e,c)) {
+        var r = new RegExp('(\\s|^)'+c+'(\\s|$)');
+        e.className = e.className.replace(r,' ');
+    }
+};
+function insertAfter(target, node) {
+    if ((typeof(target) == 'undefined') || (target == null)){
+        console.log(arguments.callee.caller);
+    } else if ((typeof(target.parentNode) != 'undefined') && (typeof(target.nextSibling) != 'undefined')) {
+        target.parentNode.insertBefore( node, target.nextSibiling);
+    }
+};
+function createElement(type, id, classname, textContent) {
+    obj = document.createElement(type);
+    if (id!=null) {
+        obj.setAttribute('id',id);
+    }
+    if ((typeof classname !== 'undefined') && classname && (classname !== '')) {
+        obj.setAttribute('class',classname);
+    }
+    if (textContent) {
+        if (classname && classname.split(' ').indexOf('noCtrlF') !== -1) {
+            obj.setAttribute('data-text', textContent);
+        } else {
+            obj.textContent = textContent;
+        }
+    }
+    return obj;
+};
 
 // for applying VESUtils.css
 function injectCSS(css) {
@@ -30,7 +70,7 @@ function injectCSS(css) {
     if (head) {
         head.appendChild(style);
     }
-}
+};
 
 
 var modules = new Array();
@@ -46,7 +86,7 @@ var VESUtils = {
         inbox: /^https?:\/\/(?:[\-\w\.]+\.)?voat\.co\/messaging\/inbox\//i,
         commentReplies: /^https?:\/\/(?:[\-\w\.]+\.)?voat\.co\/messaging\/commentreplies\//i,
         postReplies: /^https?:\/\/(?:[\-\w\.]+\.)?voat\.co\/messaging\/postreplies\//i,
-        //coments:
+        comments: /^https?:\/\/(?:[\-\w\.]+\.)?voat\.co\/v\/([\w\.\+]+)\/comments\/([\d]+)+/i,
         user: /^https?:\/\/(?:[\-\w\.]+\.)?voat\.co\/user\/([\w\.\+]+)/i,
         //submit: 
         subverse: /^https?:\/\/(?:[\-\w\.]+\.)?voat\.co\/v\/([\w\.\+]+)/i,
@@ -56,6 +96,30 @@ var VESUtils = {
     isVoat: function() {
         var currURL = location.href;
         return VESUtils.regexes.all.test(currURL);
+    },
+    isMatchURL: function() {
+        if (!VESUtils.isVoat()) {
+            return false;
+        }
+        var module = modules[moduid];
+        if (!module) {
+            console.warn("isMatchURL could not find module", moduid)
+            return false;
+        }
+
+        var exclude = module.exclude,
+            include = module.include;
+        return VESUtils.matchesPageLocation(include, exclude);
+    },
+    matchesPageLocation: function() {
+        includes = typeof includes === 'undefined' ? [] : [].concat(includes);
+        excludes = typeof excludes === 'undefined' ? [] : [].concat(excludes);
+
+        var excludesPageType = excludes.length && (RESUtils.isPageType.apply(RESUtils, excludes) || RESUtils.matchesPageRegex.apply(RESUtils, excludes));
+        if (!excludesPageType) {
+            var includesPageType = !includes.length || RESUtils.isPageType.apply(RESUtils, includes) || RESUtils.matchesPageRegex.apply(RESUtils, includes);
+            return includesPageType;
+        }
     },
     pageType: function() {
         // TODO - add code to prevent multiple runs of function for each module
@@ -72,6 +136,8 @@ var VESUtils = {
             pageType = 'postreplies';
         } else if (VESUtils.regexes.user.test(currURL)) {
             pageType = 'user';
+        } else if (VESUtils.regexes.comments.test(currURL)) {
+            pageType = 'comments';
         } else if (VESUtils.regexes.subverse.test(currURL)) {
             pageType = 'subverse';
         } else if (VESUtils.regexes.subverseListing.test(currURL)) {
@@ -82,7 +148,10 @@ var VESUtils = {
         return pageType + " -- " + currURL;
     },
     isPageType: function(/*type1,type2*/) {
-
+        var page = RESUtils.pageType();
+        return Array.prototype.slice.call(arguments).some(function(e) {
+            return (e === 'all') || (e === thisPage);
+        });
     },
     currentSubverse: function(check) {
         if (typeof this.curSub === 'undefined') {
@@ -110,47 +179,6 @@ var VESUtils = {
         return '-webkit-' + css + ';' + '-o-' + css + ';' + '-moz-' + css + ';'
             + '-ms-' + css + ';' + css + ';';
     },
-    // some basic utils
-    hasClass: function(e,c) {
-        if ((typeof(e) == 'undefined') || (e == null)) {
-            console.log(arguments,callee,caller);
-            return false;
-        }
-        return ele.className.match(new RegExp('(\\s|^)'+c+'(\\s|$)'));
-    },
-    addClass: function(e,c) {
-        if (!hasClass(e,c)) e.className += " "+c;
-    },
-    removeClass: function(e,c) {
-        if (hasClass(e,c)) {
-            var r = new RegExp('(\\s|^)'+c+'(\\s|$)');
-            e.className = e.className.replace(r,' ');
-        }
-    },
-    insertAfter: function(target, node){
-        if ((typeof(target) == 'undefined') || (target == null)){
-            console.log(arguments.callee.caller);
-        } else if ((typeof(target.parentNode) != 'undefined') && (typeof(target.nextSibling) != 'undefined')) {
-            target.parentNode.insertBefore( node, target.nextSibiling);
-        }
-    },
-    createElement: function(type, id, classname, textContent){
-        obj = document.createElement(type);
-        if (id!=null) {
-            obj.setAttribute('id',id);
-        }
-        if ((typeof classname !== 'undefined') && classname && (classname !== '')) {
-            obj.setAttribute('class',classname);
-        }
-        if (textContent) {
-            if (classname && classname.split(' ').indexOf('noCtrlF') !== -1) {
-                obj.setAttribute('data-text', textContent);
-            } else {
-                obj.textContent = textContent;
-            }
-        }
-        return obj;
-    },
 
 };
 
@@ -167,12 +195,11 @@ var VESConsole = {
     },
 };
 
-/* TODO - MODULE API
-
+/* MODULES
 IDEAS:
 + post edited highlighter
-+ ...
-
++ user highlighter
++ username hider
 */
 
 modules['VESDebugger'] = {
@@ -185,13 +212,14 @@ modules['VESDebugger'] = {
     isEnabled: function() {
         return VESConsole.getModulePrefs(this.moduid);
     },
-    //include:
-    //exclude:
+    // include: [
+    //     'all'
+    // ],
     isMatchURL: function() {
         return VESUtils.isMatchURL(this.moduid);
     },
     go: function() {
-        if ((this.isEnabled) && (this.isMatchURL)) {
+        if ((this.isEnabled) && (this.isMatchURL())) {
             // do some basic logging.
             console.log('done: ' + Date());
             console.log('isVoat: ' + VESUtils.isVoat());
@@ -211,15 +239,59 @@ modules['KeyboardNavigator'] = {
     isEnabled: function() {
         return VESConsole.getModulePrefs(this.moduid);
     },
-    //include:
-    //exclude:
+    // include: [
+    //     'all'
+    // ],
     isMatchURL: function() {
         return VESUtils.isMatchURL(this.moduid);
     },
     go: function() {
-        if ((this.isEnabled() && (this.isMatchURL)) {
+        if ((this.isEnabled() && this.isMatchURL())) {
 
         }
+    },
+};
+
+modules['VoatingNeverEnds'] = {
+    moduid: 'VoatingNeverEnds',
+    moduleName: 'Voating Never Ends',
+    description: 'Load the next page of Voat automatically.',
+    options: {
+        autoLoad: {
+            value: true,
+            description: 'Autoload next page on scroll (click to load if off)'
+        },
+        fadeDuplicates: {
+            value: true,
+            description: 'Fade any duplicate entries'
+        }
+    },
+    isEnabled: function() {
+        return VESConsole.getModulePrefs(this.moduid);
+    },
+    // include: [
+    //     'all'
+    // ],
+    // exclude: [
+    //     'comments'
+    // ],
+    isMatchURL: function() {
+        return VESUtils.isMatchURL(this.moduid);
+    },
+    go: function() {
+        if ((this.isEnabled() && this.isMatchURL())) {
+
+        }
+    },
+    getNextPrevLinks: function(e) {
+        e = e || document.body;
+        var links = {
+            next: e.querySelector('.content .pagination-container a[rel~=next]'),
+            prev: e.querySelector('.content .pagination-container a[rel~=prev]'),
+        };
+
+        if (!(links.next || links.prev)) links = false;
+        return links;
     },
 };
 
@@ -233,4 +305,3 @@ modules['KeyboardNavigator'] = {
     injectCSS(VESUtils.css);
 
 })();
-    
