afcf43806ff3616dfd25b293a63a68f9f5a0fad0
Travis Grammer <travisjgrammer@gmail.com>
2015-05-17 15:35:15 -0400

    substantiated massive chunk of voatingNeverEnds

--- voat-enhancement-suite.user.js
+++ voat-enhancement-suite.user.js
@@ -325,6 +325,35 @@ var VESUtils = {
     currentUserProfile: function() {
         // TODO
     },
+    getXYpos: function (obj) {
+        var topValue= 0,leftValue= 0;
+        while(obj){
+            leftValue+= obj.offsetLeft;
+            topValue+= obj.offsetTop;
+            obj= obj.offsetParent;
+        }
+        finalvalue = { 'x': leftValue, 'y': topValue };
+        return finalvalue;
+    },
+    elementInViewport: function (obj) {
+        // check the headerOffset - if we've pinned the subreddit bar, we need to add some pixels so the "visible" stuff is lower down the page.
+        var headerOffset = this.getHeaderOffset();
+        var top = obj.offsetTop - headerOffset;
+        var left = obj.offsetLeft;
+        var width = obj.offsetWidth;
+        var height = obj.offsetHeight;
+        while(obj.offsetParent) {
+            obj = obj.offsetParent;
+            top += obj.offsetTop;
+            left += obj.offsetLeft;
+        }
+        return (
+            top >= window.pageYOffset &&
+            left >= window.pageXOffset &&
+            (top + height) <= (window.pageYOffset + window.innerHeight - headerOffset) &&
+            (left + width) <= (window.pageXOffset + window.innerWidth)
+        );
+    },
     stripHTML: function(str) {
         var regex = /<\/?[^>]+>/gi;
         str = str.replace(regex, '');
@@ -366,9 +395,6 @@ var VESUtils = {
         btn = btn || 0;
         evt.initMouseEvent('mousedown', true, true, window.wrappedJSObject, 0, 1, 1, 1, 1, false, false, false, false, button, null);
         obj.dispatchEvent(evt);
-    },
-    elementInViewport: function(obj) {
-        // TODO
     },
     elementUnderMouse: function(obj) {
         // TODO
@@ -420,7 +446,7 @@ var VESConsole = {
         //console.log("resetModulePrefs(): resetting module prefs");
         prefs = {
             'debug': true,
-            'voatingNeverEnds': true,
+            'voatingNeverEnds': false,
             'singleClick': true,
         };
         this.setModulePrefs(prefs);
@@ -492,7 +518,7 @@ IDEAS:
 + keyboard navigator
 */
 
-modules['debug'] = {
+modules.debug = {
     moduid: 'debug',
     moduleName: 'VES Debugger',
     description: 'VES analytics for debugging.',
@@ -519,11 +545,12 @@ modules['debug'] = {
             console.log('loggedInUser: ' + VESUtils.loggedInUser());
             console.log('pageType: ' + VESUtils.pageType());
             console.log('subverse: ' + VESUtils.currentSubverse());
+            console.log('isDarkMode: ' + VESUtils.isDarkMode());
         }
     },
 };
 
-modules['voatingNeverEnds'] = {
+modules.voatingNeverEnds = {
     moduid: 'voatingNeverEnds',
     moduleName: 'Voating Never Ends',
     description: 'Load the next page of Voat automatically.',
@@ -551,9 +578,110 @@ modules['voatingNeverEnds'] = {
     },
     go: function() {
         if ((this.isEnabled()) && (this.isMatchURL())) {
-            VESUtils.addCSS();
-            // getNextPrevLinks();
-            // loadNewPage();
+            if (typeof(modules.voatingNeverEnds.dupeHash) == 'undefined') modules.voatingNeverEnds.dupeHash = {};
+            var entries = document.body.querySelectorAll('a.comments');
+            for (var i = entries.length - 1; i > -1; i--) {
+                modules.voatingNeverEnds.dupeHash[entries[i].href] = 1;
+            }
+
+            VESUtils.addCSS('#VNEloadmorebutton {}');
+
+            this.allLinks = document.body.querySelectorAll('.sitetable div.submission');
+            switch (this.options.hideDupes.value) {
+                case 'fade':
+                    VESUtils.addCSS('.VNEdupe { opacity: 0.3 }');
+                    break;
+                case 'hide':
+                    VESUtils.addCSS('.VNEdupe { display: none }');
+                    break;
+            }
+            VESUtils.addCSS('#loadingIndicator {}');
+            var nextPrevLinks = modules.voatingNeverEnds.getNextPrevLinks();
+            if (nextPrevLinks) {
+                var nextLink = nextPrevLinks.next;
+                if (nextLink) {
+                    this.nextPageURL = nextLink.getAttribute('href');
+                    // var nextXY
+                    // this.nextPageScrollY
+
+                    // this.attachLoaderWidget();
+                }
+                // TODO watch if they're returning
+                if (this.options.returnToPrevPage.value) {
+                    this.returnToPrevPageCheck(location.hash);
+                }
+
+                // watch for scrolling to the page's end
+                if (this.options.autoLoad.value && nextLink) {
+                    window.addEventListener('scroll', VESUtils.debounce.bind(VESUtils, 'scroll.voatingNeverEnds', 300, modules.voatingNeverEnds.handleScroll), false);
+                }
+
+            }
+            // TODO check for mail
+        }
+    },
+    pageMarkers: [], // page separators
+    pageURLs: [],
+    // TODO togglePause: function() {},
+    // TODO returnToPrevPageCheck: function(hash) {},
+    handleScroll: function(e) {
+        var thisPageNum = 1;
+
+        for (var i = 0, len = modules.voatingNeverEnds.pageMarkers.length; i<len; i++) {
+            var thisXY = VESUtils.getXYpos(modules.voatingNeverEnds.pageMarkers[i]);
+            if (thisXY.y < window.pageYOffset) {
+                thisPageNum = modules.voatingNeverEnds.pageMarkers[i].getAttribute('id').replace('page-','');
+            } else {
+                break;
+            }
+        }
+        var thisPageType = VESUtils.pageType()+'.'+VESUtils.currentSubverse();
+        console.log("thisPageType: " + thisPageType);
+        VESStorage.setItem('VESmodules.voatingNeverEnds.lastPage.'+thisPageType, modules.voatingNeverEnds.pageURLs[thisPageNum]);
+        var urlParams = VESUtils.getURLParams();
+        if (thisPageNum != urlParams.VNEpage) {
+            if (thisPageNum > 1) {
+                urlParams.VNEpage = thisPageNum;
+                modules.voatingNeverEnds.pastFirstPage = true;
+            } else {
+                urlParams.VNEpage = null;
+            }
+            if (modules.voatingNeverEnds.pastFirstPage) {
+                var qs = '?';
+                var count = 0;
+                var and = '';
+                for (i in urlParams) {
+                    count++;
+                    if (urlParams[i] !== null) {
+                        if (count == 2) and = '&';
+                        qs += and+i+'='+urlParams[i];
+                    }
+                }
+                // delete query parameters if there are none to display so we don't just show a ?
+                if (qs == '?') {
+                    qs = location.pathname;
+                }
+                window.history.replaceState(thisPageNum, "thepage="+thisPageNum, qs);
+            }
+        }
+        if (modules.voatingNeverEnds.fromBackButton !== true) {
+            for (var i=0, len=modules.voatingNeverEnds.allLinks.length; i<len; i++) {
+                if (VESUtils.elementInViewport(modules.voatingNeverEnds.allLinks[i])) {
+                    var thisClassString = modules.voatingNeverEnds.allLinks[i].getAttribute('class');
+                    var thisClass = thisClassString.match(/id-t[\d]_[\w]+/);
+                    if (thisClass) {
+                        var thisID = thisClass[0];
+                        var thisPageType = VESUtils.pageType()+'.'+VESUtils.currentSubverse();
+                        VESStorage.setItem('VESmodules.voatingNeverEnds.lastVisibleIndex.'+thisPageType, thisID);
+                        break;
+                    }
+                }
+            }
+        }
+        if ((VESUtils.elementInViewport(modules.voatingNeverEnds.loadingIndicator)) && (modules.voatingNeverEnds.fromBackButton !== true)) {
+            if (modules.voatingNeverEnds.isPaused !== true) {
+                modules.voatingNeverEnds.loadNewPage();
+            }
         }
     },
     getNextPrevLinks: function(e) {
@@ -566,25 +694,146 @@ modules['voatingNeverEnds'] = {
         if (!(links.next || links.prev)) links = false;
         return links;
     },
-    loadNewPage: function() {
-        // fromBackButton
+    duplicateCheck: function(newHTML){
+        var newLinks = newHTML.querySelectorAll('div.link');
+        for(var i = newLinks.length - 1; i > -1; i--) {
+            var newLink = newLinks[i];
+            var thisCommentLink = newLink.querySelector('a.comments').href;
+            if( modules.voatingNeverEnds.dupeHash[thisCommentLink] ) {
+                // console.log('found a dupe: ' + newLink.querySelector('a.title').innerHTML);
+              // let's not remove it altogether, but instead dim it...
+              // newLink.parentElement.removeChild(newLink);
+              addClass(newLink, 'VNEdupe');
+            } else {
+                modules.voatingNeverEnds.dupeHash[thisCommentLink] = 1;
+            }
+        }
+        return newHTML;
+    },
+    attachLoaderWidget: function() {
+        // add a widget at the bottom that will be used to detect that we've scrolled to the bottom, and will also serve as a "loading" bar...
+        this.loadingIndicator = document.createElement('a');
+        this.loadingIndicator.innerHTML = 'Voating Never Ends... [load more ▼]';
+        this.loadingIndicator.id = 'loadingIndicator';
+        this.loadingIndicator.className = 'btn-whoaverse btn-block voatingNeverEnds';
+        this.loadingIndicator.addEventListener('click', function(e) {
+            e.preventDefault();
+            modules.voatingNeverEnds.loadNewPage();
+        }, false);
+        insertAfter(this.siteTable, this.loadingIndicator);
+    },
+    loadNewPage: function(fromBackButton, reload) {
+        var me = modules.voatingNeverEnds;
+        if (fromBackButton) {
+            // TODO
+        } else {
+            this.fromBackButton = false;
+        }
         if (this.isLoading !== true) {
-            // change loading indicator
+            this.loadingIndicator.removeEventListener('click', me.loadNewPage, false);
+            this.loadingIndicator.innerHTML = 'Sit tight...';
             this.isLoading = true;
             GM_xmlhttpRequest({
                 method: "GET",
-                //url: '',
+                url:    this.nextPageURL,
                 onload: function(response) {
-                    // crazy http response processing/injection
+                    if ((typeof(me.loadingIndicator.parentNode) != 'undefined') && (me.loadingIndicator.parentNode !== null)) {
+                        me.loadingIndicator.parentNode.removeChild(me.loadingIndicator);
+                    }
+                    var thisHTML = response.responseText;
+                    var tempDiv = document.createElement('div');
+                    // clear javascript from tempDiv
+                    tempDiv.innerHTML = thisHTML.replace(/<script(.|\s)*?\/script>/g, '');
+                    // get the sitetable
+                    var newHTML = tempDiv.querySelector('.sitetable');
+                    if (newHTML) {
+                        var stMultiCheck = tempDiv.querySelectorAll('.sitetable');
+                        if (stMultiCheck.length == 2) {
+                            console.log('Skipped a sitetable');
+                            newHTML = stMultiCheck[1];
+                        }
+                        newHTML.setAttribute('ID','sitetable-'+me.currPage+1);
+                        me.duplicateCheck(newHTML);
+                        // check for new mail?
+                        // load other post-modifying modules
+                        if ((nextPrevLinks) && (nextPrevLinks.length)) {
+                            if (isNaN(me.currPage)) me.currPage = 1;
+                            if (!fromBackButton) me.currPage++;
+                            if ((!(me.fromBackButton)) && (me.options.returnToPrevPage.value)) {
+                                me.pageURLs[me.currPage] = me.nextPageURL;
+                                var thisPageType = VESUtils.pageType()+'.'+VESUtils.currentSubverse();
+                                VESStorage.setItem('VESmodules.voatingNeverEnds.lastPage.'+thisPageType, me.nextPageURL);
+                                // let's not change the hash anymore now that we're doing it on scroll.
+                                // location.hash = 'page='+me.currPage;
+                            }
+                            var nextLink = nextPrevLinks[nextPrevLinks.length-1];
+                            var pageMarker = createElementWithID('div','page-'+me.currPage);
+                            addClass(pageMarker,'NERPageMarker');
+                            pageMarker.innerHTML = 'Page ' + me.currPage;
+                            me.siteTable.appendChild(pageMarker);
+                            me.pageMarkers.push(pageMarker);
+                            me.siteTable.appendChild(newHTML);
+                            me.isLoading = false;
+                            if (nextLink) {
+                                // console.log(nextLink);
+                                if (nextLink.getAttribute('rel').indexOf('prev') != -1) {
+                                    // remove the progress indicator from the DOM, it needs to go away.
+                                    me.progressIndicator.style.display = 'none';
+                                    var endOfReddit = createElementWithID('div','endOfReddit');
+                                    endOfReddit.innerHTML = 'You\'ve reached the last page available.  There are no more pages to load.';
+                                    me.siteTable.appendChild(endOfReddit);
+                                    window.removeEventListener('scroll', me.handleScroll, false);
+                                }else {
+                                    // console.log('not over yet');
+                                    me.nextPageURL = nextLink.getAttribute('href');
+                                }
+                            }
+                            me.allLinks = document.body.querySelectorAll('#siteTable div.thing');
+                            if ((fromBackButton) && (me.options.returnToPrevPage.value)) {
+                                me.modalWidget.style.display = 'none';
+                                me.modalContent.style.display = 'none';
+                                // window.scrollTo(0,0)
+                                // VESUtils.scrollTo(0,me.nextPageScrollY);
+                                var thisPageType = VESUtils.pageType()+'.'+VESUtils.currentSubreddit();
+                                var lastTopScrolledID = VESStorage.getItem('VESmodules.voatingNeverEnds.lastVisibleIndex.'+thisPageType);
+                                var lastTopScrolledEle = document.body.querySelector('.'+lastTopScrolledID);
+                                if (!lastTopScrolledEle) {
+                                    var lastTopScrolledEle = newHTML.querySelector('#siteTable div.thing');
+                                }
+                                thisXY=VESUtils.getXYpos(lastTopScrolledEle);
+                                VESUtils.scrollTo(0, thisXY.y);
+                                me.fromBackButton = false;
+                            }
+                        } else {
+                            me.VNEFail();
+                        }
+                    } else {
+                        var noresults = tempDiv.querySelector('#noresults');
+                        var noresultsfound = (noresults) ? true : false;
+                        me.VNEFail(noresultsfound);
+                    }
+                },
+                onerror: function(err) {
+                    me.VNEFail();
                 }
             });
         } else {
             console.log("load new page ignored");
         }
     },
+    VNEFail: function(noresults) {
+        modules.neverEndingReddit.isLoading = false;
+        var newHTML = createElementWithID('div','VNEFail');
+        if (noresults) {
+            newHTML.innerHTML = 'Voat says there\'s nothing here.';
+        } else {
+            console.log('Voat didn\'t give a response, it may be under heavy load.');
+        }
+        modules.neverEndingReddit.siteTable.appendChild(newHTML);
+    },
 };
 
-modules['singleClick'] = {
+modules.singleClick = {
     moduid: 'singleClick',
     moduleName: 'Single Click',
     description: 'Adds an [l+c] link that opens a link and the comments page in new tabs for you in one click.',
@@ -628,10 +877,10 @@ modules['singleClick'] = {
                 VESUtils.addCSS('.VESSingleClick:hover {text-decoration: underline }');
             }
             // watch for changes to .sitetable, then reapply
-            //VESUtils.watchForElement('sitetable', modules['singleClick'].applyLinks);
+            //VESUtils.watchForElement('sitetable', modules.singleClick.applyLinks);
             document.body.addEventListener('DOMNodeInserted', function(event) {
                 if ((event.target.tagName == 'DIV') && (event.target.getAttribute('class') == 'sitetable')) {
-                    modules['singleClick'].applyLinks();
+                    modules.singleClick.applyLinks();
                 }
             }, true);
         }
@@ -670,13 +919,13 @@ modules['singleClick'] = {
                         if(e.button != 2) {
                             // check if it's a relative link (no http://voat.co) because chrome barfs on these when creating a new tab...
                             var thisLink = this.getAttribute('thisLink');
-                            if (modules['singleClick'].options.openOrder.value == 'commentsfirst') {
+                            if (modules.singleClick.options.openOrder.value == 'commentsfirst') {
                                 if (this.getAttribute('thisLink') != this.getAttribute('thisComments')) {
                                     // console.log('open comments');
                                     window.open(this.getAttribute('thisComments'));
                                 }
                                 window.open(this.getAttribute('thisLink'));
-                            } else { // modules['singleClick'].options.openOrder.value == 'linkfirst'
+                            } else { // modules.singleClick.options.openOrder.value == 'linkfirst'
                                 window.open(this.getAttribute('thisLink'));
                                 if (this.getAttribute('thisLink') != this.getAttribute('thisComments')) {
                                     // console.log('open comments');
